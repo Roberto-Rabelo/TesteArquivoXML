3fa349718d7990247a2bf8834f049aaf156645ad
Roberto Rabelo <roberto.junior.gonzaga@gmail.com>
2022-03-10 10:46:14 -0300

    update

--- testeXML/Program.cs
+++ testeXML/Program.cs
@@ -40,47 +40,7 @@ namespace testeXML
             editSheetInterfreight(sheetInterfreight, tbDeclacao, DtAdicao, dtPagamento, dtMercadoria, numItem);
             editSheetCusto(sheetCusto, tbDeclacao, DtAdicao, dtMercadoria,numItem);
             package.SaveAs(new FileInfo(@"C:\Users\infis001061\Documents\Roberto\Projetos\TesteArquivoXML\testeXML\Excel\sampleEpplus.xlsx"));
-            //document.Load(@"C:\Users\INFIS001061\Documents\Roberto\XML DA DI EXEMPLO 5.xml");
 
-
-            //var xdocResposta = XDocument.Parse(document.OuterXml);
-
-
-
-            //            DataRow row = testeXML.Tables[0].Rows[0];
-            //            DataTable tabelaAdicao = testeXML.Tables["adicao"];
-            //            //DataTable tabelaMercadoria = testeXML.Tables[4];
-
-
-
-            //            var testeDoteste = row["icms"];
-            //            string frase = "Gosto\n muito de\n C#";
-            //            Console.WriteLine(frase);
-            //            // remove as quebras de linhas
-            //            frase = frase.Replace("\n", "");
-            //            frase = frase.Replace("\r", "");
-            //            string ttt = "01600";
-            //            decimal teste = decimal.Parse(ttt) / 10000;
-            //            Console.WriteLine(frase);
-            //            DataTable tabelaMercadoria = testeXML.Tables[4];
-            //            foreach (DataRow rowMercadoria in tabelaMercadoria.Rows)
-            //            {
-            //                Console.WriteLine("quantidade:" + int.Parse(rowMercadoria["quantidade"].ToString()) / 100000);
-            //                Console.WriteLine("Valor Unitario:" + decimal.Parse(rowMercadoria["valorUnitario"].ToString()) / 10000000);
-            //                //Console.WriteLine("Decimal :" + decimal.Parse(int.Parse(rowMercadoria["quantidade"].ToString()).ToString()));
-            //                //Console.WriteLine( rowMercadoria["quantidade"].ToString().TrimStart('0').Count()); 
-            //            }
-
-
-            //            var td =
-            //                 (from tbAdicao in tabelaAdicao.AsEnumerable()
-            //                  join tbMercadoria
-            //in tabelaMercadoria.AsEnumerable() on tbAdicao["adicao_Id"] equals tbMercadoria["adicao_Id"]
-            //                  select (tbAdicao, tbMercadoria)).ToList();
-            //            //.ToList();
-            //            //DataRow rr = row.Rows[0];cargaPesoBruto
-
-            //            var t = td[0].tbMercadoria;
 
             Console.WriteLine("Hello World!");
         }
